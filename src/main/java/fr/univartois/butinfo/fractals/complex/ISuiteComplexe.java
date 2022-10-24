2bbf236cdda1fa8012c102a0b5e8906086f7dbdb
Clement.mahieux1 <clement_mahieux1@ens.univ-artois.fr>
2022-10-24 14:45:36 +0200

    commit des operations

---
+++ src/main/java/fr/univartois/butinfo/fractals/complex/ISuiteComplexe.java
@@ -0,0 +1,6 @@
+package fr.univartois.butinfo.fractals.complex;
+
+public interface ISuiteComplexe {
+    void comportementSuite1();
+}
+
---
+++ src/main/java/fr/univartois/butinfo/fractals/complex/SuiteComplexe1.java
@@ -0,0 +1,9 @@
+package fr.univartois.butinfo.fractals.complex;
+
+public class SuiteComplexe1 implements ISuiteComplexe{
+    @Override
+    public void comportementSuite1() {
+
+    }
+}
+
